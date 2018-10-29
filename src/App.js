8beddfc85523764f69fb050cd640447e81742fd8
Son Truong <son.truong@copart.com>
2018-10-29 14:17:13 -0700

    added client for Marvel api call, able to make api calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import md5 from 'md5';
+import { publicKey, privateKey } from './constants/apikeys'
+
+const client = {
+  marvel: axios.create({
+    baseURL: 'https://gateway.marvel.com:443/v1/public/',
+    responseType: 'json',
+  }),
+}
 
 class App extends Component {
+  componentDidMount() {
+    const ts = new Date().getTime()
+    const hash = md5(`${ts}${privateKey}${publicKey}`) //https://developer.marvel.com/documentation/authorization
+    client.marvel.get('/characters', {
+      params: {
+        name: 'HULK',
+        ts,
+        apikey: publicKey,
+        hash,
+      }})
+    .then(function (response) {
+      console.log(response);
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -19,6 +44,9 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
         </header>
       </div>
     );
---
+++ src/constants/apikeys.js
@@ -0,0 +1,2 @@
+export const publicKey = 'd24431c3aa826e05a1a745e8b64fa7e3'
+export const privateKey = '5d472558621f5ecbf7c6495094ce4e531bcf939a'

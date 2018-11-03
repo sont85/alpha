813cc7089cd5b5c011921dfc2e78d97cb87d2ead
Ken Truong <kenvantruong@gmail.com>
2018-11-03 12:49:09 -0700

    Got tab and search working

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-import { TextField } from '@material-ui/core';
-
+import { TextField, BottomNavigation, BottomNavigationAction } from '@material-ui/core';cd
 import md5 from 'md5';
 import { publicKey, privateKey } from './constants/apikeys'
 
-const client = {
-  marvel: axios.create({
-    baseURL: 'https://gateway.marvel.com:443/v1/public/',
-    responseType: 'json',
-    params: {
-      apikey: publicKey,
-    }
-  }),
+const client = axios.create({
+  baseURL: 'https://gateway.marvel.com:443/v1/public/',
+  responseType: 'json',
+  params: {
+    apikey: publicKey,
+  }
+})
+
+const getHash = () => {
+  const ts = new Date().getTime()
+  const hash = md5(`${ts}${privateKey}${publicKey}`)
+  return { hash, ts }
 }
 
 class App extends Component {
@@ -23,36 +26,38 @@ class App extends Component {
     this.state = {
       characterName: null,
       characterList: [],
-      descriptionInfo: null
+      descriptionInfo: null,
+      tabPage: 0,
     }
   }
   componentDidMount() {
-    const ts = new Date().getTime()
-    const hash = md5(`${ts}${privateKey}${publicKey}`) //https://developer.marvel.com/documentation/authorization
-    client.marvel.get('/characters', {
+    this.fetchCharacterList('a')
+  }
+  fetchCharacterList(searchText) {
+    const { ts, hash } = getHash()
+    client.get('/characters', { 
       params: {
         ts,
         hash,
         limit: 50,
+        nameStartsWith: searchText,
       }
     }).then((response) => {
       console.log(response)
       this.setState({
-        characterList: response.data.data.results.map((char) => {
-          return char.name
-        })
+        characterList: response.data.data.results
       })
     })
   }
 
   fetchCharacters(name) {
-    const ts = new Date().getTime()
-    const hash = md5(`${ts}${privateKey}${publicKey}`)
-    client.marvel.get('/characters', {
+    const { ts, hash } = getHash()
+    client.get('/characters', {
       params: {
         name,
         ts,
         hash,
+        
       }
     }).then((response) => {
       console.log(response)
@@ -70,30 +75,56 @@ class App extends Component {
     const name = e.target.value
     this.fetchCharacters(name)
   }
-
-// ----description
-  handleDescriptionChange = (e) => {
-    const description = e.target.value
-    this.fetchDescription(description)
-  }
+  handleTabChange = (event, tabPage) => {
+    this.setState({ tabPage: tabPage });
+  };
 
   render() {
     return (
       <div>
-        <TextField
-          type="text"
-          label="Characters Name"
-          variant="filled"
-          style={{ backgroundColor: '#e6ceff' }}
-          onChange={this.handleCharactersChange}
-        />
-        <h1>{this.state.characterName}</h1>
-        <img src={this.state.imageUrl} />
-        <p>{this.state.descriptionInfo}</p>
-
-        {this.state.characterList.map((character) => (
-          <li>{character}</li>
-        ))}
+        {this.state.tabPage === 0 &&
+          <div>
+            <TextField
+              type="text"
+              label="Characters Name"
+              variant="filled"
+              style={{ backgroundColor: '#e6ceff' }}
+              onChange={(e) => {
+                this.fetchCharacterList(e.target.value)
+              }}
+            />
+            <h1>{this.state.characterName}</h1>
+            <img src={this.state.imageUrl} />
+            <p>{this.state.descriptionInfo}</p>
+            {['a', 'b', 'c', 'd', 'z'].map((letter) => {
+              return (
+                <a
+                  style={{ color: 'blue', cursor: 'pointer', margin: '5px' }}
+                  onClick={() => this.fetchCharacterList(letter)} >{letter}
+                </a>
+              )
+            })}
+            {this.state.characterList.map((character) => (
+              <li>{character.name}</li>
+            ))}
+          </div>
+        }
+        {this.state.tabPage === 1 &&
+          <div>PAGE 1</div>
+        }
+        {this.state.tabPage === 2 &&
+          <div>PAGE 2</div>
+        }
+        
+        <BottomNavigation
+          value={this.state.tabPage}
+          onChange={this.handleTabChange}
+          showLabels
+        >
+          <BottomNavigationAction label="Recents" />
+          <BottomNavigationAction label="Favorites" />
+          <BottomNavigationAction label="Nearby" />
+        </BottomNavigation>
       </div>
     );
   }
